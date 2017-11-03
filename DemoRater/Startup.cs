584bd17c93ac42dc08efbef11fee851e9b027c85
Jay Jackson <jaynickjackson@gmail.com>
2017-11-03 10:14:39 -0400

    initial commit

---
+++ DemoRater/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoRater.Startup))]
+namespace DemoRater
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
